d854ecef8e2af32e6417c698723bb751d0743b75
Chakradhar Kommoju <119922493+Chakradhar-0208@users.noreply.github.com>
2025-06-20 14:45:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const input=document.getElementById("txtar")
-const ch=document.getElementById('chars')
-const wr=document.getElementById('words')
-let ar
-input.addEventListener("input",()=>{
-    
-    let wrd=0
-        ch.innerText=input.value.length.toString();
-        ar=input.value.toString().trim().split(/\s+/)
-        wr.innerText=((input.value.trim()=="")?0:ar.length).toString()
-        
-        
-    
-})
+const input = document.getElementById("txtar")
+const ch = document.getElementById('chars')
+const wr = document.getElementById('words')
+let ar;
 
+const count = () => {
+
+    var wrd = 0
+    ch.innerText = input.value.length.toString();
+    ar = input.value.toString().trim().split(/\s+/)
+    wr.innerText = ((input.value.trim() == "") ? 0 : ar.length).toString()
+
+}
+
+input.addEventListener("input", count)
+
+const resetBtn = () => {
+    
+    input.value = "";
+    count()
+    
+}
